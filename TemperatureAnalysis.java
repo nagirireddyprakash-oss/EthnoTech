d2e5c75ce923cccb07bbbbe031ad9bd580dc384f
nagirireddyprakash-oss <nagirireddyprakash@gmail.com>
2026-05-30 16:58:33 +0530

    Add files via upload

---
+++ TemperatureAnalysis.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class TemperatureAnalysis
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        double[] temp = new double[30];
+
+        double highest;
+        double lowest;
+        double sum = 0;
+
+        System.out.println("Enter temperatures for 30 days:");
+
+        for(int i = 0; i < 30; i++)
+        {
+            System.out.print("Day " + (i + 1) + ": ");
+            temp[i] = sc.nextDouble();
+        }
+
+        highest = temp[0];
+        lowest = temp[0];
+
+        for(int i = 0; i < 30; i++)
+        {
+            if(temp[i] > highest)
+            {
+                highest = temp[i];
+            }
+
+            if(temp[i] < lowest)
+            {
+                lowest = temp[i];
+            }
+
+            sum += temp[i];
+        }
+
+        double average = sum / 30;
+
+        System.out.println("\nHighest Temperature : " + highest + "°C");
+        System.out.println("Lowest Temperature  : " + lowest + "°C");
+        System.out.println("Average Temperature : " + average + "°C");
+
+        System.out.println("\nTemperatures Greater Than 35°C:");
+
+        for(int i = 0; i < 30; i++)
+        {
+            if(temp[i] > 35)
+            {
+                System.out.println("Day " + (i + 1) + " : " + temp[i] + "°C");
+            }
+        }
+
+        sc.close();
+    }
+}
